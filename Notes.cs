409d7c40154d1d5c78f7fdefff0ae5b188b01e08
hughfuchsen <hughfuchsen@gmail.com>
2026-01-06 16:51:22 +1100

    .

--- Notes.cs
+++ Notes.cs
@@ -1,3 +1,3 @@
 // need to obtain which building/level/room each character is in upon start
 // then i need to handle corresponding ignoring of triggers depending on where the character is situated
-// not made 06.01.2026
+// note made 06.01.2026
